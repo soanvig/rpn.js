31e287c2ced3a2d9f643b975eac4a36dcf279938
Soanvig <soanvig@gmail.com>
2018-02-10 14:36:56 +0100

    Update infixToRPN tests

--- tests/infixToRPN.test.js
+++ tests/infixToRPN.test.js
@@ -3,53 +3,69 @@ import infixToRPN from '@/infixToRPN';
 describe('infixToRPN', () => {
   describe('when provided empty string', () => {
     test('should return empty array', () => {
-      expect(infixToRPN('')).toContainEqual([]);
+      expect(infixToRPN('')).toEqual(expect.arrayContaining([]));
     });
   });
 
   describe('when provided not string', () => {
     test('should throw "InvalidEquation" error', () => {
-      expect(infixToRPN(123)).toThrowError('InvalidEquation');
+      function proceed () {
+        infixToRPN(123);
+      }
+
+      expect(proceed).toThrowError('InvalidEquation');
     });
   });
 
   describe('when provided single number as string', () => {
     test('should return array with that number as number', () => {
-      expect(infixToRPN('2')).toContainEqual([2]);
+      expect(infixToRPN('2')).toEqual(expect.arrayContaining([2]));
     });
   });
 
   describe('when provided single operand as string', () => {
     test('should throw "InvalidEquation" error', () => {
-      expect(infixToRPN('*')).toThrowError('InvalidEquation');
+      function proceed () {
+        infixToRPN('*');
+      }
+
+      expect(proceed).toThrowError('InvalidEquation');
     });
   });
 
   describe('when provided equation with number of ( not equal to number of )', () => {
     test('should throw "InvalidEquation" error', () => {
-      expect(infixToRPN('((2+53)')).toThrowError('InvalidEquation');
-      expect(infixToRPN('2+53)')).toThrowError('InvalidEquation');
+      function proceed1 () {
+        infixToRPN('((2+53)');
+      }
+
+      function proceed2 () {
+        infixToRPN('2+53)');
+      }
+
+      expect(proceed1).toThrowError('InvalidEquation');
+      expect(proceed2).toThrowError('InvalidEquation');
     });
   });
 
   describe('when provided equation without spaces', () => {
     describe('when equation has no ()', () => {
       test('should return good result', () => {
-        expect(infixToRPN('2+2')).toContainEqual([2, 2, '+']);
-        expect(infixToRPN('2+2*2')).toContainEqual([2, 2, 2, '*', '+']);
+        expect(infixToRPN('2+2')).toEqual(expect.arrayContaining([2, 2, '+']));
+        expect(infixToRPN('2+2*2')).toEqual(expect.arrayContaining([2, 2, 2, '*', '+']));
       });
     });
 
     describe('when provided equation with ()', () => {
       test('should return good result', () => {
-        expect(infixToRPN('2*(2+2)')).toContainEqual([2, 2, '+', '*']);
+        expect(infixToRPN('2*(2+2)')).toEqual(expect.arrayContaining([2, 2, 2, '+', '*']));
       });
     });
   });
 
   describe('when provided equation with spaces', () => {
     test('should return good result', () => {
-      expect(infixToRPN('2 + 2')).toContainEqual([2, 2, '+']);
+      expect(infixToRPN('2 + 2')).toEqual(expect.arrayContaining([2, 2, '+']));
     });
   });
 });
