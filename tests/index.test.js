9d21b2498f660177bdd43a941c66aa007ed83372
Soanvig <soanvig@gmail.com>
2018-02-10 23:48:12 +0100

    Format index tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,47 +2,45 @@ import infixToRPN from '@/infixToRPN';
 import calculateRPN from '@/calculateRPN';
 
 describe('infixToRPN + calculateRPN', () => {
-  describe('equation processed by infixToRPN and provided to calculateRPN', () => {
-    test('should evaluate from "2 + 2" to 4', () => {
-      const equation = '2 + 2';
-      const rpn = infixToRPN(equation);
-      const result = calculateRPN(rpn);
-      expect(result).toBe(4);
-    });
+  test('should evaluate from "2 + 2" to 4', () => {
+    const equation = '2 + 2';
+    const rpn = infixToRPN(equation);
+    const result = calculateRPN(rpn);
+    expect(result).toBe(4);
+  });
 
-    test('should evaluate from "2 * 2 + 2" to 6', () => {
-      const equation = '2 * 2 + 2';
-      const rpn = infixToRPN(equation);
-      const result = calculateRPN(rpn);
-      expect(result).toBe(6);
-    });
+  test('should evaluate from "2 * 2 + 2" to 6', () => {
+    const equation = '2 * 2 + 2';
+    const rpn = infixToRPN(equation);
+    const result = calculateRPN(rpn);
+    expect(result).toBe(6);
+  });
 
-    test('should evaluate from "2 * (2 + 2)" to 8', () => {
-      const equation = '2 * (2 + 2)';
-      const rpn = infixToRPN(equation);
-      const result = calculateRPN(rpn);
-      expect(result).toBe(8);
-    });
+  test('should evaluate from "2 * (2 + 2)" to 8', () => {
+    const equation = '2 * (2 + 2)';
+    const rpn = infixToRPN(equation);
+    const result = calculateRPN(rpn);
+    expect(result).toBe(8);
+  });
 
-    test('should evaluate from "2^2" to 4', () => {
-      const equation = '2^2';
-      const rpn = infixToRPN(equation);
-      const result = calculateRPN(rpn);
-      expect(result).toBe(4);
-    });
+  test('should evaluate from "2^2" to 4', () => {
+    const equation = '2^2';
+    const rpn = infixToRPN(equation);
+    const result = calculateRPN(rpn);
+    expect(result).toBe(4);
+  });
 
-    test('should evaluate from "2^(2+2)" to 16', () => {
-      const equation = '2^(2+2)';
-      const rpn = infixToRPN(equation);
-      const result = calculateRPN(rpn);
-      expect(result).toBe(16);
-    });
+  test('should evaluate from "2 ^ (2 + 2)" to 16', () => {
+    const equation = '2 ^ (2 + 2)';
+    const rpn = infixToRPN(equation);
+    const result = calculateRPN(rpn);
+    expect(result).toBe(16);
+  });
 
-    test('should evaluate from "(4 % 3) * 2" to 2', () => {
-      const equation = '(4 % 3) * 2';
-      const rpn = infixToRPN(equation);
-      const result = calculateRPN(rpn);
-      expect(result).toBe(2);
-    });
+  test('should evaluate from "(4 % 3) * 2" to 2', () => {
+    const equation = '(4 % 3) * 2';
+    const rpn = infixToRPN(equation);
+    const result = calculateRPN(rpn);
+    expect(result).toBe(2);
   });
 });
